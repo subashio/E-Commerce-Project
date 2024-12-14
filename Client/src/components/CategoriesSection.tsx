ddf8d5da8386d32cfda0a1090137ed627c15ea96
M Subash <105859085+Git-Subash@users.noreply.github.com>
2024-12-14 19:00:50 +0530

    update-maded-2

--- Client/src/components/CategoriesSection.tsx
+++ Client/src/components/CategoriesSection.tsx
@@ -23,24 +23,25 @@ export default function CategoriesSection() {
       <h1 className="text-center text-3xl font-bold">Shop by Category</h1>
 
       <div className="grid grid-cols-2 md:grid-cols-4">
-        {categories.map((_item, index) => (
-          <div
-            key={index}
-            className="basis-1/2 p-4 sm:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-          >
-            <Link to={_item.to} className="h-full">
-              <Card className="flex h-full flex-col items-center justify-center gap-6 border-none p-4 shadow-none">
-                <img
-                  src={_item.image}
-                  className="mx-auto h-28 w-28 sm:h-36 sm:w-36"
-                />
-                <p className="text-sm font-bold text-secondary/70 dark:text-secondary-foreground">
-                  {_item.name}
-                </p>
-              </Card>
-            </Link>
-          </div>
-        ))}
+        {categories &&
+          categories?.map((_item, index) => (
+            <div
+              key={index}
+              className="basis-1/2 p-4 sm:basis-1/3 lg:basis-1/4 xl:basis-1/5"
+            >
+              <Link to={_item.to} className="h-full">
+                <Card className="flex h-full flex-col items-center justify-center gap-6 border-none p-4 shadow-none">
+                  <img
+                    src={_item.image}
+                    className="mx-auto h-28 w-28 sm:h-36 sm:w-36"
+                  />
+                  <p className="text-sm font-bold text-secondary/70 dark:text-secondary-foreground">
+                    {_item.name}
+                  </p>
+                </Card>
+              </Link>
+            </div>
+          ))}
       </div>
     </MaxWidthWrapper>
   );
