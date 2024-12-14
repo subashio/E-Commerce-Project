de09699764bb62c73968c0638e01b9878484d271
M Subash <105859085+Git-Subash@users.noreply.github.com>
2024-12-14 19:04:10 +0530

    update-maded-3

--- Client/src/components/BestSellers.tsx
+++ Client/src/components/BestSellers.tsx
@@ -42,7 +42,7 @@ export default function BestSellers() {
       </div>
       <div className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-3 lg:grid-cols-4">
         {products &&
-          products.map((item, index) => (
+          products?.map((item, index) => (
             <ProductCard
               key={index}
               discount={item.discount}
