b2d78600ab138b37e23ee143b8c73ad669e686aa
M Subash <105859085+Git-Subash@users.noreply.github.com>
2024-12-23 22:41:13 +0530

    deploy/for-vercel

--- Client/src/constants/SummaryApi.ts
+++ Client/src/constants/SummaryApi.ts
@@ -1,6 +1,6 @@
 // export const baseURL = "http://localhost:3000";
-// export const baseURL = "https://server-sc9f.onrender.com";
-export const baseURL = "https://e-commerce-multi-vendor-backend.vercel.app";
+export const baseURL = "https://server-sc9f.onrender.com";
+// export const baseURL = "https://e-commerce-multi-vendor-backend.vercel.app";
 
 export const SummaryApi = {
   uploadImage: {
